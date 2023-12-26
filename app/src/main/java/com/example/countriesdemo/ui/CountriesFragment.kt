076b664f4114e5141fbfdc534b6124b54ed3fe79
Andrew Carrell <andrew.carrell@techconsulting.net>
2023-12-26 11:38:51 -0500

    Further refinements

--- app/src/main/java/com/example/countriesdemo/ui/CountriesFragment.kt
+++ app/src/main/java/com/example/countriesdemo/ui/CountriesFragment.kt
@@ -1,8 +1,6 @@
 package com.example.countriesdemo.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +10,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.Adapter.StateRestorationPolicy
 import com.example.countriesdemo.R
 import com.example.countriesdemo.databinding.FragmentCountryBinding
 import com.example.countriesdemo.ui.CountriesViewModel.Effect
@@ -23,7 +22,11 @@ class CountriesFragment : Fragment(R.layout.fragment_country) {
         FragmentCountryBinding.inflate(layoutInflater)
     }
     private val viewModel: CountriesViewModel by viewModels()
-    private val adapter: CountriesAdapter by lazy { CountriesAdapter() }
+    private val adapter: CountriesAdapter by lazy {
+        CountriesAdapter().apply {
+            stateRestorationPolicy = StateRestorationPolicy.PREVENT_WHEN_EMPTY
+        }
+    }
     private val layoutManager: LinearLayoutManager by lazy {
         LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
     }
@@ -35,17 +38,16 @@ class CountriesFragment : Fragment(R.layout.fragment_country) {
         }.root
     }
 
-    @SuppressLint("NotifyDataSetChanged")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewLifecycleOwner.lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
                     viewModel.state.collect { uiState ->
                         with(binding) {
                             progressBar.visibility = if (uiState.loading) View.VISIBLE else View.GONE
                             adapter.setData(uiState.countries)
-                            adapter.notifyDataSetChanged()
+                            adapter.notifyItemRangeChanged(0, uiState.countries.size)
                         }
                     }
                 }
@@ -61,24 +63,7 @@ class CountriesFragment : Fragment(R.layout.fragment_country) {
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putParcelable(LAYOUT_MANAGER_STATE, binding.countriesList.layoutManager?.onSaveInstanceState())
-    }
-
-    override fun onViewStateRestored(savedInstanceState: Bundle?) {
-        super.onViewStateRestored(savedInstanceState)
-        @Suppress("DEPRECATION")
-        savedInstanceState?.getParcelable<Parcelable>(LAYOUT_MANAGER_STATE)?.run {
-            binding.countriesList.layoutManager?.onRestoreInstanceState(this)
-        }
-    }
-
     private fun showMessage(message: String) {
         Snackbar.make(requireActivity().window.decorView, message, Snackbar.LENGTH_LONG).show()
-    }
-
-    companion object {
-        private const val LAYOUT_MANAGER_STATE = "classname.recycler.layout"
     }
 }
